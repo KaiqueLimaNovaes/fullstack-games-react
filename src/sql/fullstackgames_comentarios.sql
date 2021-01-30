e776c616653c2e38f19630b2ef3a5559e32c34f8
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-01-29 23:31:08 -0300

    Att db

--- src/sql/fullstackgames_comentarios.sql
+++ src/sql/fullstackgames_comentarios.sql
@@ -16,31 +16,28 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `produtos`
+-- Table structure for table `comentarios`
 --
 
-DROP TABLE IF EXISTS `produtos`;
+DROP TABLE IF EXISTS `comentarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `produtos` (
+CREATE TABLE `comentarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `categoria` varchar(100) DEFAULT NULL,
-  `descricao` varchar(500) DEFAULT NULL,
-  `preco` float DEFAULT NULL,
-  `preco_venda` float DEFAULT NULL,
-  `imagem` varchar(100) DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `comentario` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `produtos`
+-- Dumping data for table `comentarios`
 --
 
-LOCK TABLES `produtos` WRITE;
-/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
-INSERT INTO `produtos` VALUES (1,'consoles','Playstation 5',5990,4600,'ps5.png'),(2,'consoles','Playstation 4',2990,2400,'ps4.png'),(3,'consoles','Xbox series X',5990,4990,'seriesX.png'),(4,'consoles','Xbox series S',3990,2990,'seriesS.png'),(5,'consoles','Xbox one',2990,2400,'one.png'),(6,'jogos','Demon Souls Ps5',399,349,'dsPs5.png'),(7,'jogos','Spider man PS5',399,349,'spPs5.png'),(8,'jogos','Sackboy Ps5',319,299,'scPs5.png'),(9,'jogos','Cyberpunk 2077 Ps4',299,249,'cyPs4.png'),(10,'jogos','Rdr 2 Xbox one',199,149,'rd2Xbox.png'),(11,'jogos','Project cars 3 Xbox',199,169,'pc3Xbox.png'),(12,'acessorios','Controle Xbox series',599,499,'controleXboxSeries.png'),(13,'acessorios','Controle Ps5',599,499,'controlePs5.png'),(14,'acessorios','headset Ps5',699,599,'headsetPs5.png'),(15,'pc','Pc gamer',7499,6790,'pcgamer.png'),(16,'pc','Notebook gamer Asus',5990,4990,'ngamer.png');
-/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
+LOCK TABLES `comentarios` WRITE;
+/*!40000 ALTER TABLE `comentarios` DISABLE KEYS */;
+INSERT INTO `comentarios` VALUES (1,'kaique','Loja super confiavel');
+/*!40000 ALTER TABLE `comentarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -52,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-29 23:30:18
+-- Dump completed on 2021-01-29 23:27:52
